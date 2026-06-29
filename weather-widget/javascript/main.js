4c30205016a1505d6548657cf091b0211947b5d8
alb2376-stack <alb2376@columbia.edu>
2026-06-29 03:45:42 -0400

    change

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -4,7 +4,7 @@ let btn = document.querySelector(".search-button");
 
 getWeatherData = (zip) => {
   let API_KEY = config.WEATHER_API_KEY;
-  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=11106&APPID8e26bbc7f31fb19a506a614c56962fdc`;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=8e26bbc7f31fb19a506a614c56962fdc`;
 
   fetch(API_ENDPOINT)
     .then((response) => response.json())
