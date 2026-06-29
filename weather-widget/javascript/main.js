05c44fc1c588b24ed6773757e272ec1a32bddd57
alb2376-stack <alb2376@columbia.edu>
2026-06-29 04:23:45 -0400

    Update main.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -27,18 +27,8 @@ let CITY_TEMP = document.querySelector(".temperature");
       );
       // manipulate the temperature content
       CITY_TEMP.textContent = weather_in_celsius + " C"
-      let image = document.querySelector("img");
-
-     const getWeatherData = (zip) => {
-        fetch(API_ENDPOINT)
-        .then(response => response.json())
-        .then(data => {
-       let WEATHER_ICON = local_weather_data.weather[0].icon
-       image.setAttribute('src', `https://openweathermap.org/img/wn/${WEATHER_ICON}@2x.png`)
   });
 }
-    form.reset();
-    input.focus();
 
 const getZipcode = e => {
   e.preventDefault();
