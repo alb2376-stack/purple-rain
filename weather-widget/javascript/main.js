87b72bb2fa876c749db743e613366ffd6bb0c9ea
alb2376-stack <alb2376@columbia.edu>
2026-06-29 03:38:06 -0400

    go

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,1 +1,23 @@
-console.log("this works");
+console.log("this works")
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
+
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=11106&APPID8e26bbc7f31fb19a506a614c56962fdc`;
+
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let local_weather_data = data;
+        console.log(local_weather_data)
+    })
+  }
+
+const getZipCode = (e) => {
+  e.preventDefault();
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode);
